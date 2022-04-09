3b4b22af61c6509742e41485f40d0f1b2ae34530
silentkiller2009 <kumaram.vipinam@gmail.com>
2022-04-09 16:00:24 +0530

    Final3

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
-PROJECT_NAME = "Perceptron_pypi"
+PROJECT_NAME = "Perceptron-pypi"
 USER_NAME = "silentkiller2009"
 
 setuptools.setup(
